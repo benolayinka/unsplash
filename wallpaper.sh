fd88289163df1a83c96175c25afae92218d61f23
Ben Olayinka <ben.olayinka@gmail.com>
2018-02-09 11:43:43 +0100

    Fixed local reference
    Used an ugly Tilde expansion to remove hardcoded reference to my own username

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 APP_ID="cf2d94c048d80161efce7ea422d4d5fd74530778086ef7bb31f989922d9048eb"
-dest="/Users/Ben1/Pictures/Unsplash.jpg"
+dest=~/Pictures/Unsplash.jpg
+search="abstract"
 
-curl -L "https://api.unsplash.com/photos/random/?client_id=$APP_ID&orientation=landscape&query=abstract" | jq -r '.urls.raw' | xargs wget -O "$dest"
+curl -L "https://api.unsplash.com/photos/random/?client_id=$APP_ID&orientation=landscape&query=$search" | jq -r '.urls.raw' | xargs wget -O "$dest"
 
 scr='tell application "Finder" to set desktop picture to POSIX file "'"$dest"'"'
 osascript -e "$scr" 
