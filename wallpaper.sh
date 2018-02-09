223befabe1799b03160837f4162c6ce3a9715388
Ben Olayinka <ben.olayinka@gmail.com>
2018-02-09 15:59:03 +0100

    Removed jq and wget dependencies
    Forgot that these don't come standard with os x. Now depends on python.... can remove in future

--- wallpaper.sh
+++ wallpaper.sh
@@ -3,8 +3,7 @@ APP_ID="cf2d94c048d80161efce7ea422d4d5fd74530778086ef7bb31f989922d9048eb"
 dest=~/Pictures/Unsplash.jpg
 search="abstract"
 
-curl -L "https://api.unsplash.com/photos/random/?client_id=$APP_ID&orientation=landscape&query=$search" | jq -r '.urls.raw' | xargs wget -O "$dest"
-
+curl -L "https://api.unsplash.com/photos/random/?client_id=$APP_ID&orientation=landscape&query=$search" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["urls"]["raw"]' | xargs curl -o "$dest"
 scr='tell application "Finder" to set desktop picture to POSIX file "'"$dest"'"'
 osascript -e "$scr" 
 
